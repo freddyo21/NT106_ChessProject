e58ae37fbb7629b5061d1fb3063c6223702af34f
freddyo21 <freddy.preo21@gmail.com>
2026-04-19 01:09:49 +0700

    refactor: database pool mini fix

--- backend/src/config/database.config.ts
+++ backend/src/config/database.config.ts
@@ -1,18 +1,21 @@
 import { createClient } from "@supabase/supabase-js";
 import { Pool } from "pg";
 
+const isProduction = process.env.NODE_ENV === "production";
 export const pool = new Pool(
     {
-        ...process.env.DATABASE_URL ? { connectionString: process.env.DATABASE_URL } : {
-            host: process.env.DB_HOST || "localhost",
-            port: Number(process.env.DB_PORT || 5432),
-            user: process.env.DB_USER || "postgres",
-            password: process.env.DB_PASSWORD || "postgres",
-            database: process.env.DB_NAME || "chess_db",
-        },
-        ssl: {
-            rejectUnauthorized: process.env.NODE_ENV === "production",
-        }
+        ...(process.env.DATABASE_URL && process.env.DATABASE_URL !== ""
+            ? { connectionString: process.env.DATABASE_URL }
+            : {
+                host: process.env.DB_HOST || "localhost",
+                port: Number(process.env.DB_PORT || 5432),
+                user: process.env.DB_USER,
+                password: process.env.DB_PASSWORD,
+                database: process.env.DB_NAME,
+            }),
+        ssl: isProduction
+            ? { rejectUnauthorized: false }
+            : false
     }
 );
 

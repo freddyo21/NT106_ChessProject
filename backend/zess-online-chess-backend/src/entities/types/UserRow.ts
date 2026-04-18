b86a9581c2909a71484b905f93a6ce7e65dda5f6
freddyo21 <freddy.preo21@gmail.com>
2026-04-18 10:30:34 +0700

    chore: Initialize monorepo workspace and configurations

--- backend/zess-online-chess-backend/src/entities/types/UserRow.ts
+++
@@ -1,9 +0,0 @@
-export type UserRow = {
-    id: number;
-    name: string;
-    email: string;
-    password_hash: string;
-    elo: number;
-    created_at: string;
-    updated_at: string;
-};
---
+++ shared/src/index.ts
@@ -0,0 +1,4 @@
+export * from "./inferred-types"
+export * from "./schemas";
+export * from "./types";
+export * from "./utils";
